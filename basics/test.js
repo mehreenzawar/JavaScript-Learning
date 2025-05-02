6c6a7e4eac23fc84022e3f483cabb0f3126c500a
mehreenzawar <mehreenzawar234@gmail.com>
2025-05-02 16:33:23 +0000

    Learning File

---
+++ basics/test.js
@@ -0,0 +1,7 @@
+const accountid = 12345
+let accountemail = "mehr@gmail.com"
+let accountpassword = "2345"
+accountcity = "ccw"
+
+
+console.table([accountid, accountemail, accountpassword, accountcity]);
