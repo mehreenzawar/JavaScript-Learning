d567600aae2fd1f6a9868cd29e4ac777b2300ae2
mehreenzawar <mehreenzawar234@gmail.com>
2025-05-15 16:03:21 +0000

    data

---
+++ basics/datatype.js
@@ -0,0 +1,26 @@
+//Primitive:
+//Number, string, boolean, null, undefined, symbol, BbigInt
+
+//const score = 100
+//const scorevalue = 100.3
+
+//const isLoggedIn = false
+//const outsidetemp = null
+//let useremail;
+
+//const id = Symbol('123')
+//const anotherId = Symbol('123')
+//console.log(id == anotherId);
+
+const bigNumber = 725732915379n
+
+const heros = ["Quaid", "Iqbal", "Rehmat Ali"]
+let myObj = {
+    name: "heros",
+    age: 22,
+}
+const myFunction = function(){
+    console.log(hello);
+}
+
+console.log(typeof myFunction);
