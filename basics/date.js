9f021a649782c1eab4ff4c074bf233a6a563b86e
mehreenzawar <mehreenzawar234@gmail.com>
2025-05-17 05:00:16 +0000

    date

---
+++ basics/date.js
@@ -0,0 +1,26 @@
+let myDate = new Date()
+/*console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString())
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.toTimeString());*/
+
+let myCreatedDate = new Date(2000, 1, 6)
+console.log(myCreatedDate.toDateString());
+
+let myTimestamp = Date.now()
+console.log(myTimestamp);
+console.log(myCreatedDate.getTime());
+
+
+
+
+
+
+
+
+
